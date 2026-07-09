f3154eafbfc10c5923a7df4f5ce9935de0a4799d
Arnold Kiritu <arnoldkells4@gmail.com>
2026-07-09 21:27:12 +0300

    the table and object constructor are in place
    the display is now comprehensive and the structure looking good

--- scripts.js
+++ scripts.js
@@ -5,7 +5,6 @@ function Book(title, author, pages, read, id) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.id = id;
 
   this.info = function(){
     return `${this.title} by ${this.author}, pages ${this.pages}. ${this.read ? "Yes read" : "Not read yet"}`
@@ -18,7 +17,49 @@ function addBookToLibrary(title, author, pages, read, id) {
   myLibrary.push(newBook);
 }
 
-addBookToLibrary();
+addBookToLibrary("Hobbit", "A.K.Doe", "600", "Yes");
 
 console.log(myLibrary);
 
+const table = document.querySelector("table");
+const row = document.querySelector(".table-row");
+
+
+function displayBooks(){
+  myLibrary.forEach(book => {
+    const bookProperties = [book.title, book.author, book.pages, book.read];
+
+    bookProperties.forEach(text => {
+      const cell = document.createElement("td");
+
+      cell.textContent = text;
+      row.append(cell);
+    });
+
+    table.append(row);
+  });
+}
+
+displayBooks();
+
+// const tableBody = document.querySelector(".table-body");
+
+// const displayBooks = () => {
+//   tableBody.innerHTML = "";
+
+//   myLibrary.forEach(book => {
+//     const row = document.createElement("tr");
+
+//     const bookPropertie = [book.title, book.author, book.pages, book.read];
+
+//     bookPropertie.forEach(text => {
+//       const cell = document.createElement("td");
+//       cell.textContent = text;
+//       row.append(cell);
+//     });
+
+//     tableBody.append(row);
+//   });
+// }
+
+// displayBooks();
